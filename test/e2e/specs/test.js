c834a934758c0c87af1bdd3e58ed4a9c93a536d0
Max Mathews <mmathews@ifishgroup.com>
2018-03-02 17:01:07 -0800

    updated test file > e2e

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -27,6 +27,7 @@ module.exports = {
       .waitForElementPresent('i[class=ti-unlock]', 5000)
       .assert.elementPresent('i[class=ti-unlock]')
   },
+
   'SA can Add User and User can Log In' : function (browser) {
     browser
       .click('i[class=ti-user]')
@@ -54,9 +55,9 @@ module.exports = {
       .click('button[type=submit]')
       .waitForElementPresent('i[class=ti-layers]', 5000)
       .assert.elementPresent('i[class=ti-layers]')
-
   },
-  'Author can create KA' : function (browser) {
+
+  'Author can create KA 1' : function (browser) {
     browser      
       .click('i[class=ti-layers')
       .useXpath()
@@ -64,7 +65,7 @@ module.exports = {
       .useCss()
       .waitForElementPresent('input[name=title]', 5000)
       .assert.elementPresent('input[name=title]')
-      .setValue('input[name=title]', 'This is a test title')
+      .setValue('input[name=title]', 'Test KA 1')
       .assert.elementPresent('div[class="CodeMirror cm-s-paper CodeMirror-wrap"]')
       .click('div[class="CodeMirror cm-s-paper CodeMirror-wrap"]')
       .click('.CodeMirror textarea').keys('This is body text')
@@ -75,20 +76,59 @@ module.exports = {
       .assert.elementPresent('div[class="toast-container toast-top-full-width"]')
       .refresh()
   },
-  'Author can edit KA' : function (browser) {
+
+  'Author can create KA 2' : function (browser) {
+    browser      
+      .waitForElementPresent('input[name=title]', 5000)
+      .assert.elementPresent('input[name=title]')
+      .setValue('input[name=title]', 'Test KA 2')
+      .assert.elementPresent('div[class="CodeMirror cm-s-paper CodeMirror-wrap"]')
+      .click('div[class="CodeMirror cm-s-paper CodeMirror-wrap"]')
+      .click('.CodeMirror textarea').keys('This is body text')
+      .assert.elementPresent('input[name=categories]')
+      .sendKeys('input[name=categories]', 'created, via, nightwatch')
+      .click('button[type=submit]')
+      .waitForElementPresent('div[class="toast-container toast-top-full-width"]', 5000)
+      .assert.elementPresent('div[class="toast-container toast-top-full-width"]')
+      .refresh()
+  },
+
+  'Author can edit KA 2' : function (browser) {
     browser
+      .waitForElementPresent('i[class=ti-layers', 5000)
       .click('i[class=ti-layers')
       .useXpath()
       .click('//a[text()="Edit"]')
-      .useCss()
-      .waitForElementPresent('a[class=article]', 5000)
-      .click('a[class=article]')
-      .useXpath()
+      .waitForElementPresent('//table/tbody/tr[2]/th[2]/a[1]/i', 5000)
+      .click('//table/tbody/tr[2]/th[2]/a[1]/i')
       .assert.elementPresent('//button[text()="Update"]')
       .useCss()
       .assert.elementPresent('input[name=title]')
       .clearValue('input[name=title]')
-      .setValue('input[name=title]', 'Author Edited title text')
+      .setValue('input[name=title]', 'Author Edited title text - KA 2')
+      .click('select[id=status')
+      .click('option[value="pending approval"]')
+      .useXpath()
+      .click('//button[text()="Update"]')
+      .useCss()
+      .waitForElementPresent('div[class="toast-container toast-top-full-width"]', 5000)
+      .assert.elementPresent('div[class="toast-container toast-top-full-width"]')
+      .refresh() 
+  },
+
+  'Author can edit KA' : function (browser) {
+    browser
+      .waitForElementPresent('i[class=ti-layers', 5000)
+      .click('i[class=ti-layers')
+      .useXpath()
+      .click('//a[text()="Edit"]')
+      .waitForElementPresent('//table/tbody/tr[1]/th[2]/a[1]/i', 5000)
+      .click('//table/tbody/tr[1]/th[2]/a[1]/i')
+      .assert.elementPresent('//button[text()="Update"]')
+      .useCss()
+      .assert.elementPresent('input[name=title]')
+      .clearValue('input[name=title]')
+      .setValue('input[name=title]', 'Author Edited title text - KA 1')
       .click('select[id=status')
       .click('option[value="pending approval"]')
       .useXpath()
@@ -100,7 +140,8 @@ module.exports = {
       .waitForElementPresent('i[class=ti-unlock]', 5000)
       .click('i[class=ti-unlock')      
   },
-  'CM can edit, Approve, and Publish KA' : function (browser) {
+
+  'CM can Reject KA' : function (browser) {
     browser
       .waitForElementPresent('i[class=ti-lock]', 5000)
       .assert.elementPresent('i[class=ti-lock]')
@@ -115,15 +156,38 @@ module.exports = {
       .click('i[class=ti-layers')
       .useXpath()
       .click('//a[text()="Edit"]')
-      .useCss()
-      .waitForElementPresent('a[class=article]', 5000)
-      .click('a[class=article]')
-      .useXpath()
+      .waitForElementPresent('//table/tbody/tr[1]/th[2]/a[1]/i', 5000)
+      .click('//table/tbody/tr[1]/th[2]/a[1]/i')
       .assert.elementPresent('//button[text()="Update"]')
       .useCss()
       .assert.elementPresent('input[name=title]')
       .clearValue('input[name=title]')
-      .setValue('input[name=title]', 'CM edited title text')
+      .setValue('input[name=title]', 'CM edited title text - KA 1 - Rejected')
+      .click('select[id=status')
+      .click('option[value="rejected"]')
+      .useXpath()
+      .click('//button[text()="Update"]')
+      .useCss()
+      .waitForElementPresent('div[class="toast-container toast-top-full-width"]', 5000)
+      .assert.elementPresent('div[class="toast-container toast-top-full-width"]')
+      .refresh()   
+  },
+
+  'CM can edit, Approve, and Publish KA' : function (browser) {
+    browser
+      .waitForElementPresent('i[class=ti-unlock]', 5000)
+      .assert.elementPresent('i[class=ti-unlock]')
+      .click('i[class=ti-layers')
+      .useXpath()
+      .click('//a[text()="Edit"]')
+      .waitForElementPresent('//table/tbody/tr[2]/th[2]/a[1]/i', 5000)
+      .click('//table/tbody/tr[2]/th[2]/a[1]/i')
+      .assert.elementPresent('//button[text()="Update"]')
+      .useCss()
+      .waitForElementPresent('input[name=title]', 5000)
+      .assert.elementPresent('input[name=title]')
+      .clearValue('input[name=title]')
+      .setValue('input[name=title]', 'CM edited title text - KA 1 - Approved')
       .click('select[id=status')
       .click('option[value="approved"]')
       .useXpath()
@@ -139,14 +203,47 @@ module.exports = {
       .click('i[class=ti-layers]')
       .useXpath()
       .click('//a[text()="Edit"]')
-      .waitForElementPresent('//table/tbody/tr/th[2]/a[2]/i', 5000)
-      .click('//table/tbody/tr/th[2]/a[2]/i')
+      .waitForElementPresent('//table/tbody/tr[2]/th[2]/a[2]/i', 5000)
+      .click('//table/tbody/tr[2]/th[2]/a[2]/i')
       .pause(1000)
       .acceptAlert()
       .useCss()
       .waitForElementPresent('i[class=ti-unlock]', 5000)
       .click('i[class=ti-unlock]')
       .assert.elementPresent('i[class=ti-lock]')   
+  },
+
+  'Author can edit KA after Rejection' : function (browser) {
+    browser
+      .waitForElementPresent('i[class=ti-lock]', 5000)
+      .click('i[class=ti-lock')
+      .assert.elementPresent('input[id=email]')
+      .setValue('input[id=email]', 'testuser@test.com')      
+      .assert.elementPresent('input[id=password]')
+      .setValue('input[id=password]', 'abcd1234!')
+      .click('button[type=submit]')
+      .waitForElementPresent('i[class=ti-layers]', 5000)
+      .assert.elementPresent('i[class=ti-layers]')
+      .click('i[class=ti-layers')
+      .useXpath()
+      .click('//a[text()="Edit"]')
+      .waitForElementPresent('//table/tbody/tr/th[2]/a[1]/i', 5000)
+      .click('//table/tbody/tr/th[2]/a[1]/i')
+      .assert.elementPresent('//button[text()="Update"]')
+      .useCss()
+      .assert.elementPresent('input[name=title]')
+      .clearValue('input[name=title]')
+      .setValue('input[name=title]', 'KA 1 - Previously Rejected')
+      .click('select[id=status')
+      .click('option[value="pending approval"]')
+      .useXpath()
+      .click('//button[text()="Update"]')
+      .useCss()
+      .waitForElementPresent('div[class="toast-container toast-top-full-width"]', 5000)
+      .assert.elementPresent('div[class="toast-container toast-top-full-width"]')
+      .refresh()
+      .waitForElementPresent('i[class=ti-unlock]', 5000)
+      .click('i[class=ti-unlock')      
   },
 
   'SA can Remove User and User can no longer Log In' : function (browser) {
