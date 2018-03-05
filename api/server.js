ddd1a94ac06ec9da3d903c529a06644d0cffc2ef
Dustin Lish <dlish27@gmail.com>
2018-03-04 20:34:08 -0700

    Lint fix and added Options request header handler

--- api/server.js
+++ api/server.js
@@ -2,30 +2,36 @@ require('./app/config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./app/db/mongoose');
+const { mongoose } = require('./app/db/mongoose');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
 const port = process.env.PORT;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Expose-Headers", 'x-auth');
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-auth");
-  next();
+app.use((req, res, next) => {
+  res.header('Access-Control-Expose-Headers', 'x-auth');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth');
+  // intercepts OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
-var routes = require('./app/routes/apiRoutes'); //importing route
-routes(app); //register the route
+const routes = require('./app/routes/apiRoutes');
+// importing route
+routes(app); // register the route
 
 app.listen(port, () => {
-  console.log(`\n Your api is running here http://localhost:${port}/api` + `\n`);
-  console.log(`\n Your api documentation is running here http://localhost:${port}/api/docs` + `\n`);
+  console.log(`\n Your api is running here http://localhost:${port}/api\n`);
+  console.log(`\n Your api documentation is running here http://localhost:${port}/api/docs\n`);
 });
 
-module.exports = {app};
+module.exports = { app };
