2a88b34631a51acf41b4303823bcb6ea72ce8b45
Dustin Lish <dlish27@gmail.com>
2018-03-04 20:32:41 -0700

    Add mongdb connect retry

--- api/app/db/mongoose.js
+++ api/app/db/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+var connectWithRetry = function () {
+  return mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
 
 module.exports = { mongoose };
