efe7ad297966b036d1c21965694e73470e78ea07
Ernie Lopez <elopez@ifishgroup.com>
2018-03-06 11:30:44 -0800

    Configured Unit Tests

--- test/unit/api.test.js
+++
@@ -1,8 +0,0 @@
-// Test checking wether Insight API is responding or not
-const request = require('./includes/request');
-
-test('Insight API server is responding', async () => {
-  expect.assertions(1);
-  const data = await request.getRawOutput('');
-  expect(data).toEqual('Insight API "api_v1" has been setup successfully.');
-});
